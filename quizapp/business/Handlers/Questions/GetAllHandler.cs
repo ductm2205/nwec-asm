078542001fbbc76c41951775985f4a0246973f8a
ductm2205 <ductranminh12342003@gmail.com>
2025-03-13 19:43:51 +0700

    feat(api.questions): implement get all questions api

--- quizapp/business/Handlers/Questions/GetAllHandler.cs
+++ quizapp/business/Handlers/Questions/GetAllHandler.cs
@@ -2,6 +2,7 @@ using business.Commands;
 using core.Models;
 using core.Models.Responses;
 using data.Infrastructures;
+using Microsoft.EntityFrameworkCore;
 
 namespace business.Handlers.Questions;
 
@@ -9,18 +10,28 @@ public class GetAllHandler(IUnitOfWork unitOfWork) : BaseHandler<GetAllCommand<Q
 {
     protected override async Task<PaginatedResult<QuestionResponse>> HandleCommand(GetAllCommand<QuestionResponse> request, CancellationToken cancellationToken)
     {
-        var questions = await _unitOfWork.QuestionRepo.GetAllAsync();
+        var questions = _unitOfWork.QuestionRepo.GetQuery().Include(q => q.Answers);
 
-        var quesResponses = questions.Select(ques => new QuestionResponse
+        var quesResponses = await questions.Select(ques => new QuestionResponse
         {
             Id = ques.Id,
             Content = ques.Content,
             QuestionType = ques.QuestionType,
             QuizId = ques.QuizId,
+            Answers = ques.Answers!.Select(
+                a => new AnswerResponse
+                {
+                    Id = a.Id,
+                    Content = a.Content,
+                    IsActive = a.IsActive,
+                    CreatedAt = a.CreatedAt,
+                    UpdatedAt = a.UpdatedAt
+                }
+            ),
             IsActive = ques.IsActive,
             CreatedAt = ques.CreatedAt,
             UpdatedAt = ques.UpdatedAt
-        }).ToList();
+        }).ToListAsync(cancellationToken: cancellationToken);
 
         return new PaginatedResult<QuestionResponse>(
             pageNumber: 1,
